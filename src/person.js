3bcc87ec3337982325295bb8fd979724ca61d053
lisaeriksson <lisa.c.eriksson@gmail.com>
2017-11-07 13:15:54 +0100

    Adding local web server, Sintra

--- src/person.js
+++ src/person.js
@@ -6,4 +6,5 @@ function Person(attr) { //"initialize" Person
 Person.prototype.calculate_bmi = function() { //instance of a person object, run calculate_bmi to test method
   calculator = new BMICalculator(); // initialize method and store it in a variable
   calculator.metric_bmi(this); //call variable and run metric_bmi on the object itself / "self"
+//  calculator.imperial_bmi(this); //call variable and run imperial_bmi on the object itself / "self"
 };
